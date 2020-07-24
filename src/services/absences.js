fb0dc36c44ad23b2855ff1124ec89fd8f0c84237
David Riera <david.riera@somenergia.coop>
2020-07-24 09:40:11 +0200

    added custom hooks for api fetch

--- src/services/absences.js
+++ src/services/absences.js
@@ -1,13 +1,167 @@
+import React, { useState, useEffect } from 'react'
+
 import axios from 'axios'
+import { useAuthState } from '../context/auth'
 
 export const API_URL = 'https://gestorabsencies-demo.somenergia.local'
 export const APP = axios.create({ baseURL: API_URL })
 
-export const absences = async (worker) => {
-  return axios.get(`${API_URL}/absencies/workers/${worker}`)
-    .then(response => {
-      console.log(response)
-      return response
-    })
-    .catch(error => error)
+export const useFetch = () => {
+  const [data, setData] = useState({})
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  const { user } = useAuthState()
+
+  const init = () => {
+    setData(null)
+    setLoading(false)
+    setError(false)
+  }
+
+  const load = async (endPoint) => {
+    init()
+    setLoading(true)
+    try {
+      const result = await axios.get(`${API_URL}${endPoint}`, {
+        headers: {
+          Authorization: `JWT ${user?.token}`
+        }
+      })
+      setData(result?.data)
+    } catch (e) {
+      setError(true)
+    }
+    setLoading(false)
+  }
+
+  return [{ data, loading, error }, load]
+}
+
+export const useFetchMember = () => {
+  const [member, setMember] = useState({})
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  const { user } = useAuthState()
+
+  const init = () => {
+    setMember(null)
+    setLoading(false)
+    setError(false)
+  }
+
+  const load = async (memberId) => {
+    init()
+    setLoading(true)
+    try {
+      const result = await axios.get(`${API_URL}/absencies/workers/${memberId}`, {
+        headers: {
+          Authorization: `JWT ${user?.token}`
+        }
+      })
+      setMember(result?.data)
+    } catch (e) {
+      setError(true)
+    }
+    setLoading(false)
+  }
+
+  return [{ member, loading, error }, load]
+}
+
+export const useFetchMembers = () => {
+  const [members, setMembers] = useState({})
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  const { user } = useAuthState()
+
+  const init = () => {
+    setMembers(null)
+    setLoading(false)
+    setError(false)
+  }
+
+  const load = async () => {
+    init()
+    setLoading(true)
+    try {
+      const result = await axios.get(`${API_URL}/absencies/workers`, {
+        headers: {
+          Authorization: `JWT ${user?.token}`
+        }
+      })
+      setMembers(result?.data)
+    } catch (e) {
+      setError(true)
+    }
+    setLoading(false)
+  }
+
+  return [{ members, loading, error }, load]
+}
+
+export const useFetchTeams = () => {
+  const [teams, setTeams] = useState({})
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  const { user } = useAuthState()
+
+  const init = () => {
+    setTeams(null)
+    setLoading(false)
+    setError(false)
+  }
+
+  const load = async () => {
+    init()
+    setLoading(true)
+    try {
+      const result = await axios.get(`${API_URL}/absencies/teams`, {
+        headers: {
+          Authorization: `JWT ${user?.token}`
+        }
+      })
+      setTeams(result?.data)
+    } catch (e) {
+      setError(true)
+    }
+    setLoading(false)
+  }
+
+  return [{ teams, loading, error }, load]
+}
+
+export const useFetchAbsencesType = () => {
+  const [types, setTypes] = useState({})
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  const { user } = useAuthState()
+
+  const init = () => {
+    setTypes(null)
+    setLoading(false)
+    setError(false)
+  }
+
+  const load = async () => {
+    init()
+    setLoading(true)
+    try {
+      const result = await axios.get(`${API_URL}/absencies/absencetype`, {
+        headers: {
+          Authorization: `JWT ${user?.token}`
+        }
+      })
+      setTypes(result?.data)
+    } catch (e) {
+      setError(true)
+    }
+    setLoading(false)
+  }
+
+  return [{ types, loading, error }, load]
 }
